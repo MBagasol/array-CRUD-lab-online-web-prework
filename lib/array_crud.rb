59d934e9513f4d59dc76d4a31be194667a6f8e7a
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-07 04:28:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  car_makes ["Mazda", "Nissan", "Toyota", "Honda"]
+  
 end
 
 def create_an_array
-  
+  car_makes ["Mazda", "Nissan", "Toyota", "Honda"]
 end
 
 def add_element_to_end_of_array(array, element)
